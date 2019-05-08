f17d3ee49f3050c243d4e1a0477b95a38fbc1913
bzhazreal <stanislas.david@mailoo.org>
2019-05-08 20:50:37 +0200

    Add install telegram support

---
+++ roles/common/scripts/install_telegram.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import tarfile
+import re
+
+# Configuration vars.
+src_directory = "/usr/local/src/"
+
+"""
+Function who return telegram setup filename.
+"""
+def get_file_name(filename):
+    files = os.listdir(filename)
+    for file in files:
+        if re.match(r"tsetup.*", file):
+            return file
+    return 0
+
+"""
+Function to extract archive.
+"""
+def extract_file(file, dest):
+    tar = tarfile.open(file, "r:xz")
+    tar.extractall("/usr/local/src/")
+    tar.close()
+
+
+# Script execution.
+file = get_file_name(src_directory)
+
+if file != 0:
+    full_path = src_directory + file
+    extract_file(full_path, ".")
+
+os.symlink(src_directory + '/Telegram/Telegram', '/usr/local/bin/telegram')
